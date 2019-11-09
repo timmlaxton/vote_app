7e7f6fd48b7961ad4957dedfa5f502a50c11aad0
Tim <timmlaxton@gmail.com>
2019-11-09 22:19:20 +0000

    Added authentication

--- server/handlers/auth.js
+++ server/handlers/auth.js
@@ -1,12 +1,19 @@
+const jwt =  require('jsonwebtoken');
+
 const db = require('../models');
 
 exports.register = async (req, res, next)  => {
   try {
     const user = await db.User.create(req.body);
-    const {id, username} = user
+    const {id, username} = user;
 
-    res.json({id, username});
+    const token = jwt.sign({id, username}, process.env.SECRET);
+
+    res.status(200).json({id, username, token});
   } catch(err) {
+    if (err.code === 11000) {
+      err.message = "Sorry that username is already in use";
+    }
     next(err);
   }
 };
@@ -18,14 +25,19 @@ exports.login = async (req,res,next) => {
     const valid = await user.comparePassword(req.body.password);
 
   if (valid){
+    const token = jwt.sign({id, username}, process.env.SECRET);
+
     res.json({
       id,
-      username
-    });
+      username,
+      token
+    })
   } else {
-      throw new Erroe('Invalid UserName/Password');
+      throw new Error();
   }
   } catch(err){
+    err.message = "Invalid Username/Password";
+
     next(err);
   }
 };
--- server/handlers/index.js
+++ server/handlers/index.js
@@ -10,7 +10,7 @@ module.exports.notFound = (req, res, next) => {
 };
 
 module.exports.errors = (err, res, next) => {
-  res.status(err.status || 500).json({
+  res.status(err.status || 400).json({
     err: err.message || "Something went bad"
   });
 };
