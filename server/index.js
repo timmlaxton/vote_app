a2f59abfed5e4fd2c36dce61132ab73859bec090
Tim <timmlaxton@gmail.com>
2019-11-09 19:34:45 +0000

    server up and running port 4000

--- server/index.js
+++ server/index.js
@@ -1,1 +1,8 @@
-console.log("hiya world");
+const express = require('express');
+
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => res.send("hiya"));
+
+app.listen(port, console.log(`Server started on port $(port)`));
