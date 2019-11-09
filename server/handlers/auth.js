eca0cddbaac0cf894b7434027dc7945585b66abd
Tim <timmlaxton@gmail.com>
2019-11-09 21:43:48 +0000

    crpt password

--- server/handlers/auth.js
+++ server/handlers/auth.js
@@ -1,4 +1,31 @@
-exports.register =(req,res,next)  => {
+const db = require('../models');
 
-  exports.login = (req,res,next) => {}
-}
+exports.register = async (req, res, next)  => {
+  try {
+    const user = await db.User.create(req.body);
+    const {id, username} = user
+
+    res.json({id, username});
+  } catch(err) {
+    next(err);
+  }
+};
+
+exports.login = async (req,res,next) => {
+  try {
+    const user = await db.User.findOne({username: req.body.username });
+    const {id, username} = user;
+    const valid = await user.comparePassword(req.body.password);
+
+  if (valid){
+    res.json({
+      id,
+      username
+    });
+  } else {
+      throw new Erroe('Invalid UserName/Password');
+  }
+  } catch(err){
+    next(err);
+  }
+};
