4de2c871773e35d882f41f78c2bb184b1efa60f2
Tim <timmlaxton@gmail.com>
2019-11-10 13:56:50 +0000

    added Restful routes

--- server/handlers/poll.js
+++ server/handlers/poll.js
@@ -2,7 +2,8 @@ const db = require('../models');
 
 exports.showPolls = async (req, res, next) => {
   try {
-    const polls = await db.Poll.find();
+    const polls = await db.Poll.find().populate
+    ('user', ['username', 'id']);
 
     res.status(200).json(polls);
   } catch (err) {
@@ -10,6 +11,19 @@ exports.showPolls = async (req, res, next) => {
     next(err);
   }
 };
+
+exports.usersPolls = async(req, res, next) => {
+  try {
+    const {id} = req.decoded;
+
+    const user = await db.User.findById(id).populate('polls');
+
+    res.status(200).json(user.polls);
+  } catch (err){
+    err.status = 400
+    next(err);
+  }
+}
 
 exports.createPoll = async (req, res, next) => {
   try {
@@ -31,4 +45,81 @@ exports.createPoll = async (req, res, next) => {
     err.status = 400;
     next(err);
   }
-}
+};
+
+exports.getPoll = async (req,res,next) => {
+  try {
+    const {id} = req.params;
+
+    const poll = await db.Poll.findById(id)
+    .populate('user', ['username', 'id']);
+
+    if (!poll) throw new Error('No poll found');
+
+    res.status(200).json(poll);
+  } catch (err) {
+    err.status = 400;
+    next(err);
+  }
+};
+
+exports.deletePoll = async (req, res, next) => {
+  try {
+    const {id: pollId} = req.params;
+    const {id: userId} = req.decoded;
+
+    const poll = await db.Poll.findbyId(pollId);
+    if (!poll) throw new Error('No poll found');
+    if (poll.user.toString() !== userId) {
+      throw new Error('Unaithorized access');
+    }
+
+    await poll.remove();
+    res.status(202).json(poll);
+  } catch(err){
+    err.status = 400
+    next(err);
+  }
+};
+
+exports.vote = async (req, res, next) => {
+  try {
+    const {id: pollId} = req.params;
+    const {id: userId} = req.decoded;
+    const {answer} = req.body;
+
+    if(answer){
+      const poll = await db.Poll.findById(pollId);
+      if(!poll) throw new Error('No poll found');
+
+    const vote = poll.options.map(
+    option => {
+      if (option.option === answer){
+        return {
+          option: option.option,
+          _id: option._id,
+          votes: options.votes  + 1
+        };
+      } else {
+      return option;
+       }
+      }
+    );
+
+    if (poll.voted.filter(user => user.toString() === userId).length <= 0){
+      poll.voted.push(userId);
+      poll.options = vote;
+      await poll.save();
+
+      res.status(202).json(poll);
+    } else {
+      throw new Error("Already voted");
+    }
+  } else {
+    throw new Error('No answer provided');
+  }
+  } catch (err) {
+    err.status = 400;
+    next(err)
+  }
+};
--- server/routes/poll.js
+++ server/routes/poll.js
@@ -6,5 +6,13 @@ const auth = require('../middlewares/auth');
 router.route('/').get(handle.showPolls)
 .post(auth, handle.createPoll);
 
+router.get('/user', auth, handle.usersPolls);
+
+router
+.route('/:id')
+.get(handle.getPoll)
+.post(auth, handle.vote)
+.delete(auth, handle.deletePoll)
+
 
 module.exports = router;
